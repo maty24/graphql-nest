170d230828b1540c116cf38bef10b47a9a9f5497
Matias Godoy <matiasgodoy@Matiass-Mac-mini.local>
2023-03-19 20:52:28 -0300

    init

---
+++ src/items/items.resolver.ts
@@ -0,0 +1,45 @@
+import { ParseUUIDPipe } from '@nestjs/common';
+import { Resolver, Query, Mutation, Args, Int, ID } from '@nestjs/graphql';
+import { ItemsService } from './items.service';
+import { Item } from './entities/item.entity';
+import { CreateItemInput, UpdateItemInput } from './dto';
+
+
+@Resolver(() => Item)
+export class ItemsResolver {
+  constructor(private readonly itemsService: ItemsService) {}
+
+  @Mutation(() => Item)
+  async createItem(
+    @Args('createItemInput') createItemInput: CreateItemInput
+    //despues de los 2 puntos se espera un item pero como aync le pongo un promise
+  ): Promise<Item> {
+    return this.itemsService.create(createItemInput);
+  }
+
+  @Query(() => [Item], { name: 'items' })
+  async findAll(): Promise<Item[]> {
+    return this.itemsService.findAll();
+  }
+
+  @Query(() => Item, { name: 'item' })
+  async findOne(
+    @Args('id', { type: () => ID }, ParseUUIDPipe ) id: string
+  ): Promise<Item> {
+    return this.itemsService.findOne(id);
+  }
+
+  @Mutation(() => Item)
+  updateItem(
+    @Args('updateItemInput') updateItemInput: UpdateItemInput
+  ):Promise<Item> {
+    return this.itemsService.update( updateItemInput.id, updateItemInput );
+  }
+
+  @Mutation(() => Item)
+  removeItem(
+    @Args('id', { type: () => ID }) id: string
+  ): Promise<Item> {
+    return this.itemsService.remove(id);
+  }
+}
